86218b576685ea9e73e57937d8fb13a8bfcb739d
telranAndroid <telran.asana@gmail.com>
2016-05-01 21:42:14 +0300

    050_Log_Menu_Action

--- app/src/main/java/com/yasam/menu/MainActivity.java
+++ app/src/main/java/com/yasam/menu/MainActivity.java
@@ -1,8 +1,10 @@
 package com.yasam.menu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printToLogs(View view) {
+        String menuItem1 = null;
+        TextView txtVwMenuItem1 = (TextView) findViewById(R.id.txtVw_menuItem1);
+        if(txtVwMenuItem1!=null)
+            menuItem1 = txtVwMenuItem1.getText().toString();
+
+        if(menuItem1!=null)
+            Log.v(this.getClass().getSimpleName(), menuItem1);
+
+
+        String menuItem2 = null;
+        TextView txtVwMenuItem2 = (TextView) findViewById(R.id.txtVw_menuItem2);
+        if(txtVwMenuItem2!=null)
+            menuItem2 = txtVwMenuItem2.getText().toString();
+
+        if(menuItem2!=null)
+            Log.v(this.getClass().getSimpleName(), menuItem2);
+
+
+        String menuItem3 = null;
+        TextView txtVwMenuItem3 = (TextView) findViewById(R.id.txtVw_menuItem3);
+        if(txtVwMenuItem3!=null)
+            menuItem3 = txtVwMenuItem3.getText().toString();
+
+        if(menuItem3!=null)
+            Log.v(this.getClass().getSimpleName(), menuItem3);
     }
 }
